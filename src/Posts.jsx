8b5c090c13df11e4b673180a210191472d1c4c48
Jaman-M <jaman.manik1@gmail.comm>
2025-01-25 07:12:31 +0800

    react core concepts recap

---
+++ src/Posts.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+
+export default function Posts() {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => setPosts(data))
+    }, [])
+
+    return (
+        <div className="">
+            <h3>Posts: {posts.length}</h3>
+        </div>
+    )
+}
+
+/**
+ * 
+ * 
+*/ 
